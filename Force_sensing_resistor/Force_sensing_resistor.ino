24c12997a0cb6deeff4e46ceefb53baffec62703
sai-y <sai@altschool.com>
2015-11-21 06:00:56 -0800

    force_sensing resistor

--- Force_sensing_resistor/Force_sensing_resistor.ino
+++ Force_sensing_resistor/Force_sensing_resistor.ino
@@ -1,13 +1,13 @@
-int FSR = A0; //the FSR is connected to analog pin 0 (A0)
+int resistor = A0; //the FSR is connected to analog pin 0 (A0)
 
 void setup(){
 Serial.begin(9600);
 }
 
 void loop(){
-int FSRValue = analogRead(FSR);//this takes a reading of the analog value of the FSR
+int value = analogRead(resistor);//this takes a reading of the analog value of the FSR
 
-Serial.println(FSRValue);//this gives a printout of the reading of the analog value that is measured
-delay(5000); //this slows down the readings so that they're given every 5 seconds
+Serial.println(value);//this gives a printout of the reading of the analog value that is measured
+delay(1000); //this slows down the readings so that they're given every 5 seconds
 }
 
