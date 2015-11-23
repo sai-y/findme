fa3dd1d832488b5533605e41038b729ffa04bb13
sai-y <sai@altschool.com>
2015-11-23 11:45:20 -0800

    Commit

--- Arduin_launchpad_message/Arduin_launchpad_message.ino
+++ Arduin_launchpad_message/Arduin_launchpad_message.ino
@@ -1,41 +1,91 @@
 #include <SoftwareSerial.h>
+#include "PulsePattern.h"
 
+#include <Process.h>
 #define PRESSURE 0
 #define BUTTON 1
 #define BUTTON_PIN 2
+#define TEMPERATURE 3
 #define PRESSURE_SENSE A0
-#define HEART_BEAT 300000
-
-SoftwareSerial sigfox(8, 7); // ARDUINO RX, TX
+#define TEMPERATURE_SENSE A1
+#define HEART_BEAT 60000
+#define RX 8
+#define TX 7
+SoftwareSerial sigfox(RX, TX); // ARDUINO RX, TX
 long lastTimeMillis = 0;
+
+uint16_t SOSpattern[] = {  
+  500,500,500,500,500,1500,  // SOS in morse
+  1500,500,1500,500,1500,1500, 
+  500,500,500,500,500,1500 };
+
+uint16_t XYZpattern[] = {  
+  100,100,100,100,100,100,  // SOS in morse
+  500,500,500,500,500,500, 
+  1000,1000,1000,1000,1000,1000 };
+
+uint8_t patternSize = 18;
+uint8_t startLevel = LOW;
+
 void setup() {
   // SETUP OF THE SOFTWARESERIAL
-  pinMode(8, INPUT);
-  pinMode(7, OUTPUT);
+  pinMode(RX, INPUT);
+  pinMode(TX, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
   sigfox.begin(9600);
   Serial.begin(9600);
-  
+  Bridge.begin();
 }
 
 void loop() {
 
+  
   if(!digitalRead(BUTTON_PIN)){
     byte flags, data = 0;
     flags |= (1<<BUTTON);
     data |= (1<<BUTTON);
+    
+    SIGFOX_SEND(flags, data,0);
+    Process p;
+    p.begin("aplay");
+    p.addParameter("/root/sos_signal.wav");
+    p.run();
 
-    SIGFOX_SEND(flags, data);
+    
+    p.begin("aplay");
+    p.addParameter("/root/Alert.wav");
+    p.run();
   //WAIT 5s BEFORE SENDING A MESSAGE AGAIN
     delay(1000);
+    PPGenerator.init(13, SOSpattern, patternSize, startLevel, PRESCALE_1024);
+    PPGenerator.start();
+    
   }
   if(analogRead(PRESSURE_SENSE)>900){
     byte flags,data =0;
     flags |= (1<<PRESSURE);
     data |= (1<<PRESSURE);
-    SIGFOX_SEND(flags, data);
+    SIGFOX_SEND(flags, data, 0);
   //WAIT 5s BEFORE SENDING A MESSAGE AGAIN
-    delay(1000);
+    delay(3000);
+  }
+  float temp_value =0;
+  for(int i=0; i<1024;i++){
+     temp_value = analogRead(TEMPERATURE_SENSE) * 5.0;
+  }
+  
+  temp_value /= 1024.0;
+  
+  byte temperature = (temp_value - 0.5) * 100;
+  if(temperature>100){
+    temperature =0;
+  }
+  Serial.println(temperature);
+  if(temperature > 50){
+    byte sensorMask =0;
+    sensorMask |= (1<<TEMPERATURE);
+    Serial.println(temperature);
+    SIGFOX_SEND(sensorMask,0, temperature);
   }
   // send every 5 mins
   if (millis() - lastTimeMillis >= HEART_BEAT) {
@@ -47,15 +97,16 @@ void loop() {
     if(analogRead(PRESSURE_SENSE>900)){
       data |= (1<<PRESSURE);
     }
-    SIGFOX_SEND(sensorMask, data);
+    SIGFOX_SEND(sensorMask, data, temperature);
     //peopleCounter = 0;
     lastTimeMillis = millis();
   }
+  
 }
   
 
 
-String SIGFOX_SEND(byte sensorMask, byte data){
+String SIGFOX_SEND(byte sensorMask, byte data, byte temperature){
   char* buf_str = (char*) malloc (10 + 1);
   String protocol="AT$SF=";
   
@@ -64,12 +115,16 @@ String SIGFOX_SEND(byte sensorMask, byte data){
 
   sprintf(buf_str, "%02X\0", data);
   protocol += String(buf_str);
+
+  sprintf(buf_str, "%02X\0", temperature);
+  protocol += String(buf_str);
   
   sprintf(buf_str, "%02X\0", 0);
-  protocol += String(buf_str);
   protocol += String(buf_str);
   protocol +="\r";
   sigfox.print(String(protocol));
   delay(1000);
   return "OK";
 }
+
+
